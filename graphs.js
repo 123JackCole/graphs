6eb97fa4d75bea465ba9c9917010f7fd166b5d68
123JackCole <123jack.cole@gmail.com>
2020-08-12 12:36:05 -0700

    added add node method

--- graphs.js
+++ graphs.js
@@ -2,3 +2,8 @@ function Graph() {
     this.AdjList = new Map();
 }
 
+Graph.prototype.addNode = function(data) {
+    // initialize an adjacency list with an empty array
+    this.AdjList.set(data, []);
+}
+
